584f7e284c5d3646830cbb33aae02ee1ca12c94f
saifularafat <saifularafat.info@gmail.com>
2023-08-11 02:02:42 +0600

    footer section is a modify

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,34 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from "../../assets/logo.png"
-// import Wave from 'react-wavify';
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
-import ceo from "../../assets/user_img.png"
 const Footer = () => {
     return (
-        <div className='relative mt-6'>
-            {/* <Wave className='h-24 absolute -top-8 ' mask="url(#mask)" fill="url(#gradient)" >
-                <defs>
-                    <linearGradient id="gradient" gradientTransform="rotate(90)">
-                        <stop offset="10%" stopColor="#1277b0" />
-                        <stop offset="90%" stopColor="#fff" />
-                    </linearGradient>
-                    <mask id="mask">
-                        <rect x="0" y="0" width="2000" height="200" fill="url(#gradient)" />
-                    </mask>
-                </defs>
-            </Wave> */}
-            <div className='px-8 grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10 space-y-6 bg-slate-100 py-16 bg-gradient-to-r from-slate-900 to-zinc-900'>
-                <div className=' text-center text-white space-y-3'>
+        <div className='relative mt-6 bg-gradient-to-r from-slate-900 to-zinc-900 pt-11'>
+            <div className='px-8 grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-10 space-y-6  border-b border-slate-800 pb-4'>
+                <div className=' text-center text-white space-y-1'>
                     <img src={logo} alt="" className='md:w-auto w-3/4 mx-auto' />
-                    <p className='text-lg '>Descriptors</p>
-                    <p className='text-lg text-justify'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis totam numquam tenetur illo iste repudiandae odit eligendi alias at ullam.</p>
-                    <div className='w-28 h-28 mx-auto rounded-xl object-cover'>
-                        <img src={ceo} alt="" className='rounded-xl' />
+                    <p className='text-sm text-justify'>It’s a special day when our beloved someone like any of our students, friends, College or University have to sit for their exams. Whether it is a exam for school, college just tell them to believe in themselves and want success and good performance.</p>
+                    <div className='md:flex items-center justify-center gap-5 text-center space-y-3'>
+                        <p className='footer_social_icon md:mt-4'>
+                            <FaFacebookF className='w-5 h-5 mx-auto md:pt-0 translate-y-1/4' />
+                        </p>
+                        <p className='footer_social_icon'>
+                            <FaTwitter className='w-5 h-5 mx-auto md:pt-0 translate-y-1/4' />
+                        </p>
+                        <p className='footer_social_icon'>
+                            <FaLinkedinIn className='w-5 h-5 mx-auto md:pt-0 translate-y-1/4' />
+                        </p>
                     </div>
                 </div>
                 <div className='text-center text-white space-y-3' >
-                    <h2 className='text-3xl font-sans font-semibold pb-4 border-b-2 border-slate-600 rounded-md md:mt-0 mt-6'>Exam Platform</h2>
+                    <h2 className='text-xl font-sans font-semibold pb-4 border-b-2 border-slate-600 rounded-md md:mt-0 mt-6'>Exam Platform</h2>
                     <p className='footer_exam'>MCQ Exam</p>
                     <p className='footer_exam'>Cheating</p>
                     <p className='footer_exam'>Student</p>
@@ -36,7 +30,7 @@ const Footer = () => {
                     <p className='footer_exam'>Testimonial</p>
                 </div>
                 <div className='text-center text-white space-y-3'>
-                    <h2 className='text-3xl font-sans font-semibold  pb-4 border-b-2 border-slate-600 rounded-md'>Who we service</h2>
+                    <h2 className='text-xl font-sans font-semibold  pb-4 border-b-2 border-slate-600 rounded-md'>Who we service</h2>
                     <div>
                         <Link to='' className='footer_res'>Schools</Link>
                     </div>
@@ -51,7 +45,7 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className='text-center text-white space-y-3'>
-                    <h2 className='text-3xl font-sans font-semibold  pb-4 border-b-2 border-slate-600 rounded-md'>Resources</h2>
+                    <h2 className='text-xl font-sans font-semibold  pb-4 border-b-2 border-slate-600 rounded-md'>Resources</h2>
                     <div className=''>
                         <Link to='' className='footer_res '>Support</Link><br />
                     </div>
@@ -64,19 +58,9 @@ const Footer = () => {
                     <div>
                         <Link to='' className='footer_res'>Technology</Link>
                     </div>
-                    <div className='md:flex items-center justify-center gap-3 md:mt-5 mt-3 text-center space-y-3'>
-                        <p className='footer_social_icon md:mt-4'>
-                            <FaFacebookF className='w-8 h-8 mx-auto md:pt-0 translate-y-1/4' />
-                        </p>
-                        <p className='footer_social_icon'>
-                            <FaTwitter className='w-8 h-8 mx-auto md:pt-0 translate-y-1/4' />
-                        </p>
-                        <p className='footer_social_icon'>
-                            <FaLinkedinIn className='w-8 h-8 mx-auto md:pt-0 translate-y-1/4' />
-                        </p>
-                    </div>
                 </div>
             </div>
+                <p className='text-center text-slate-500 py-2'>©2023 Your Organization Name The Web Titans. All rights reserved.</p>
         </div>
     );
 };
